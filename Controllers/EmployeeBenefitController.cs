41b356bdff26a4dfe7b64f6e2064ff291c758259
rian0502 <muhammadfebrianhasibuan@gmail.com>
2024-12-09 10:28:10 +0700

    menambahkan fitur untuk menambahkan benefit

--- Controllers/EmployeeBenefitController.cs
+++ Controllers/EmployeeBenefitController.cs
@@ -1,17 +1,94 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DAHAR.Helper;
+using DAHAR.Models;
+using DAHAR.ViewModels.EmployeeBenefit;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
-namespace DAHAR.Controllers
+namespace DAHAR.Controllers;
+
+public class EmployeeBenefitController(
+    AppDbContext context,
+    MongoDbContext mongoDbContext,
+    UserManager<Users> userManager) : Controller
 {
-    public class EmployeeBenefitController : Controller
+    public async Task<IActionResult> Details(int id)
     {
-        public IActionResult Create()
-        {
-            return View();
-        }
+        var employee = await context.Employee
+            .Where(e => e.EmployeeId == id)
+            .Include(e => e.Benefits)!
+            .ThenInclude(b => b.Benefit)
+            .Include(e => e.Users)
+            .FirstOrDefaultAsync();
 
-        public IActionResult Edit(int id)
+        return View(employee);
+    }
+
+    public async Task<IActionResult> Create(int id)
+    {
+        ViewBag.Benefits = await context.Benefits.ToListAsync();
+        return View(new CreateEmployeeBenefit
         {
-            return View();
+            EmployeeId = id
+        });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(CreateEmployeeBenefit model)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Benefits = await context.Benefits.ToListAsync();
+                return View(new CreateEmployeeBenefit
+                {
+                    EmployeeId = model.EmployeeId
+                });
+            }
+
+            var user = await userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return RedirectToAction("Logout", "Account");
+            }
+            var time = DateTime.UtcNow;
+            await context.EmployeeBenefits.AddAsync(new EmployeeBenefit
+            {
+                EmployeeId = model.EmployeeId,
+                BenefitId = model.BenefitId,
+                StartDate = model.StartDate,
+                EndDate = model.EndDate,
+                Amount = model.Amount
+            });
+            await context.SaveChangesAsync();
+            var logs = new AppLogModel
+            {
+                CreatedAt = time,
+                CreatedBy = $"{user.Id} - {user.FullName}",
+                Params = JsonConvert.SerializeObject(model),
+                Source = JsonConvert.SerializeObject(new
+                {
+                    Controller = "EmployeeBenefitController",
+                    Action = "Create",
+                    Database = "EmployeeBenefits"
+                })
+            };
+            await mongoDbContext.AppLogs.InsertOneAsync(logs);
+            TempData["Success"] = "Benefit added successfully";
+            return RedirectToAction("Details", "EmployeeBenefit", new { id = model.EmployeeId });
         }
+        catch (Exception e)
+        {
+            TempData["Errors"] = e.Message;
+            return RedirectToAction("Create", new { id = model.EmployeeId });
+        }
+    }
+
+    public IActionResult Edit(int id)
+    {
+        return View();
     }
 }
---
+++ ViewModels/EmployeeBenefit/CreateEmployeeBenefit.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DAHAR.ViewModels.EmployeeBenefit;
+
+public class CreateEmployeeBenefit
+{
+    [Required(ErrorMessage = "Start Date is required")]
+    public DateOnly StartDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    [Required(ErrorMessage = "End Date is required")]
+    public DateOnly EndDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    [Required(ErrorMessage = "Benefit is required")]
+    public int BenefitId { get; set; }
+    [Required(ErrorMessage = "Amount is required")]
+    public double Amount { get; set; }
+    
+    public int EmployeeId { get; set; }
+}
